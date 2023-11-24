34d6d723c40d61bd4711a94c6275ae11b76cab81
Yazdan Haider <yazdanhaider007@gmail.com>
2023-11-24 15:19:29 +0530

    Update Shop.jsx

--- client/src/shop/Shop.jsx
+++ client/src/shop/Shop.jsx
@@ -14,7 +14,7 @@ const Shop = () => {
   }, []);
   return (
     <div className="mt-28 px-4 lg:px-24">
-      <h2 className="text-5xl font-bold text-center">All Books are here</h2>
+      <h2 className="text-5xl font-bold text-center">Art Collections</h2>
 
       <div className="grid gap-8 my-12 lg:grid-cols-4 sm:grid-cols-2 md:grid-cols-3 grid-cols-1">
         {books.map((book) => (
